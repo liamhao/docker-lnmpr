0d7c8fde4683b57a9a2a68acbbde93fd540aa39d
liamhao <w736611944@gmail.com>
2020-12-28 17:51:43 +0800

    提交

--- nginx/index.php
+++ nginx/index.php
@@ -6,6 +6,16 @@ echo "<h1>{$_SERVER['SERVER_ADDR']}</h1>";
 
 var_dump($_SERVER["REQUEST_URI"]);
 
+// $ch = curl_init('order.service.consul/index.html');
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// echo '========';
+// var_dump(curl_exec($ch));
+// echo '========';
+// var_dump(curl_error($ch));
+// echo '========';
+// curl_close($ch);
+
 if (isset($_GET['hsj'])) {
   echo "<h3>参数:</h3>";
   var_dump($_GET);
